de51506cc853d36cb6fc19f6e6e16d0480be95fd
Angie Cohen <angiedev2001@gmail.com>
2023-04-11 14:05:45 -0300

    Se creo la estuctura del componente navbar y se implementaron los estilos

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,10 +1,73 @@
-import React from 'react';
-import './navbar.css'
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.jpg";
+import "./navbar.css";
+
+const Menu = () => (
+  <>
+    <p>
+      <a href="#aboutUs">Sobre Nosotros</a>
+    </p>
+    <p>
+      <a href="#contenido">Servicios</a>
+    </p>
+    <p>
+      <a href="#possibility">Cursos</a>
+    </p>
+    <p>
+      <a href="#feature">¿Por qué elegirnos?</a>
+    </p>
+    <p>
+      <a href="#blog">Contacto</a>
+    </p>
+  </>
+);
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
-}
+  const [toggleMenu, setToggleMenu] = useState(false);
 
-export default NavBar
+  return (
+    <div className="acad__navbar">
+      <div className="acad__navbar-links">
+        <div className="acad__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        <div className="acad__navbar-links_container">
+          <Menu />
+        </div>
+      </div>
+      <div className="acad__navbar-sign">
+        <p>Sign in</p>
+        <button type="button">Sign up</button>
+      </div>
+      <div className="acad__navbar-menu">
+        {toggleMenu ? 
+          <RiCloseLine
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+         : 
+          <RiMenu3Line
+            color="#fff"
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        }
+        {toggleMenu && (
+          <div className="acad__navbar-menu_container scale-up-center">
+            <div className="acad__navbar-menu_container-links">
+              <Menu />
+              <div className="acad__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
