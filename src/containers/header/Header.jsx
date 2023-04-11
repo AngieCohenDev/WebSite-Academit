eb3198290743c07cdcca2aa1ca467cdd6223acca
Angie Cohen <angiedev2001@gmail.com>
2023-04-11 14:32:21 -0300

    Primera parte de la creacion de la estructura del Header

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
-import './header.css'
+import React from "react";
+import people from "../../assets/people.png"
+import "./header.css";
+
+
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="acad__header section__padding" id="home">
+      <div className="acad__header-content">
+        <h1 className="gradient__text">
+          Somos Academit, Programando la élite del mañana, hoy
+        </h1>
+        <p>Tenemos el objetivo de preparar, actualizar, potencializar y desarrollar las capacidades de las personas para el mundo laboral a través de nuestros cursos, capacitaciones, asesorías y carreras. Otorgando acceso educativo de calidad para crear profesionales que satisfactoriamente cumplirán a cabalidad con los más altos estándares o requerimientos que existan en el mercado.</p>
+        <div className="acad__header-content_input">
+          <input type="email" placeholder="Ingrese su Email, por favor…" />
+          <button type="button">Empecemos</button>
+        </div>
+        <div className="acad__header-content__people">
+            <img src={people} alt="people" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
