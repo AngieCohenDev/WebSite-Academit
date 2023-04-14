f2b2bfd5f29ae43b079f57cab7f0d0d311348191
Angie Cohen <angiedev2001@gmail.com>
2023-04-14 15:38:33 -0300

    Primeros elementos de la seccion Por que elegirnos?

--- src/containers/contenido/Contenido.jsx
+++ src/containers/contenido/Contenido.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import {Feature} from '../../components/index';
 import './contenido.css'
+
+
 const Contenido = () => {
   return (
-    <div>Contenido</div>
+    <div className='acad__contenido section__margin' id='contenido'>
+      <div className='acad__contenido-feature'>
+        <Feature />
+      </div>
+      <div className='acad__contenido-heading'>
+        <h1 className='gradient-text'> Somos tu mejor opción para potencializar tus habilidades </h1>
+        <p>Explora nuestros cursos y servicios!</p>
+      </div>
+    </div>
   )
 }
 
