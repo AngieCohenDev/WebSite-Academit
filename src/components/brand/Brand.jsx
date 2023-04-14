f87c6af6515d79a628be23e3a8c2d5ae8cc84ed9
Angie Cohen <angiedev2001@gmail.com>
2023-04-14 14:41:28 -0300

    Se agregaron los elementos del componente Brand

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import { javascript, github, react, mysql, node } from './imports';
 import './brand.css'
+
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='acad__brand section__padding'>
+        <div>
+          <img src={javascript} alt="javascript" />
+        </div>
+        <div>
+          <img src={github} alt="github" />
+        </div>
+        <div>
+          <img src={mysql} alt="mysql" />
+        </div>
+        <div>
+          <img src={node} alt="node" />
+        </div>
+        <div>
+          <img src={react} alt="react" />
+        </div>
+    </div>
   )
 }
 
---
+++ src/components/brand/imports.js
@@ -0,0 +1,13 @@
+import javascript from '../../assets/javascript.png';
+import mysql from '../../assets/mysql.png';
+import github from '../../assets/github.png';
+import node from '../../assets/node.png';
+import react from '../../assets/react.png';
+
+export{
+    javascript,
+    mysql,
+    github,
+    node,
+    react,
+}
